0cefca9cd09c1490271a3e47d85c36c207c12155
faisaltimbangan32@gmail.com <faisaltimbangan32@gmail.com>
2022-03-27 15:39:03 +0700

    Homework-module-2-session-3

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import PlaylistContainer from './components/PlaylistContainer';
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="title"><b>Create Playlist</b></h1>
+      <PlaylistContainer />
+    </div>
+  );
+}
+
+export default App;

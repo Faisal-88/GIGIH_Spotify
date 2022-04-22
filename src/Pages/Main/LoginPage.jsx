f63cd9a5cc20f42b813bcf5e0d64be02ab038de3
faisaltimbangan32@gmail.com <faisaltimbangan32@gmail.com>
2022-04-22 09:38:06 +0700

    Final Project

---
+++ src/Pages/Main/LoginPage.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react';
+import {
+  clientID,
+  scope,
+  redirectUri,
+  publicUrl,
+} from '../../libs/values';
+import './style.css';
+
+function LoginPage() {
+  const [theme, setTheme] = useState(localStorage.getItem('theme'));
+
+  function LoginButton() {
+    const spotifyUrl = 'https://accounts.spotify.com/authorize'
+    + '?response_type=token'
+    + '&client_id=' + encodeURIComponent(clientID)
+    + '&scope=' + encodeURIComponent(scope)
+    + '&redirect_uri=' + encodeURIComponent(redirectUri);
+
+    return (
+      <div className="btn-login">
+        <a href={spotifyUrl}>
+          <i className="fab fa-spotify" />
+          LOG IN WITH SPOTIFY
+        </a>
+      </div>
+    );
+  }
+
+  useEffect(() => {
+    if (theme) {
+      document.documentElement.className = theme;
+    } else {
+      setTheme('theme-light-pink');
+      localStorage.setItem('theme', 'theme-light-pink');
+      document.documentElement.className = 'theme-light-pink';
+    }
+  }, []);
+
+  return (
+    <div className="login-page">
+      <div className="login-card">
+
+        <LoginButton />
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
